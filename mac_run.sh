a6b40517af37bcb4099782651a84418a6b5edc18
long.lyhoang <long.lyhoang@lazada.com>
2022-04-11 19:49:43 +0700

    correct commands

--- mac_run.sh
+++ mac_run.sh
@@ -3,12 +3,12 @@
 echo
 echo Starting add watermark process...
 
-xxx="Your User Name"
+xxx="long.lyhoang"
 
 # [Image Watermark + No Debug]
-python3 -B add_watermark.py --images_dir "/Users/${xxx}/Downloads/canon/2022_03_07" \
+python3 -B add_watermark.py --images_dir "/Users/${xxx}/Downloads/canon/new" \
     --watermark_path "/Users/${xxx}/Downloads/canon/Watermark-5.png" \
-    --has_gui 0
+    --has_gui 0 \
     --gimp_path "/Applications/GIMP-2.10.app/Contents/MacOS/gimp"
 
 # [Image Watermark + Debug]
@@ -16,14 +16,14 @@ python3 -B add_watermark.py --images_dir "/Users/${xxx}/Downloads/canon/2022_03_
 #     --watermark_path "/Users/${xxx}/Downloads/canon/Watermark-5.png" \
 #     --debug_image "IMG_1755.JPG" \
 #     --debug 1 \
-#     --has_gui 0
+#     --has_gui 0 \
 #     --gimp_path "/Applications/GIMP-2.10.app/Contents/MacOS/gimp"
 
 # python3 -B add_watermark.py --images_dir "/Users/${xxx}/Downloads/canon" \
 #     --watermark_path "/Users/${xxx}/Downloads/canon/BepCoVy-1.png" \
 #     --debug_image "3d83a3ef7d22b17ce833.jpg" \
 #     --debug 1 \
-#     --has_gui 0
+#     --has_gui 0 \
 #     --gimp_path "/Applications/GIMP-2.10.app/Contents/MacOS/gimp"
 
 # [Text Watermark + No Debug]
@@ -31,7 +31,7 @@ python3 -B add_watermark.py --images_dir "/Users/${xxx}/Downloads/canon/2022_03_
 #     --watermark_text "Bếp Cô Vy" \
 #     --font_name "FleurDeLeah" \
 #     --font_size 30 \
-#     --has_gui 0
+#     --has_gui 0 \
 #     --gimp_path "/Applications/GIMP-2.10.app/Contents/MacOS/gimp"
 
 # [Text Watermark + Debug]
@@ -41,7 +41,7 @@ python3 -B add_watermark.py --images_dir "/Users/${xxx}/Downloads/canon/2022_03_
 #     --font_name "TheNautigal" \
 #     --font_size 150 \
 #     --debug 1 \
-#     --has_gui 0
+#     --has_gui 0 \
 #     --gimp_path "/Applications/GIMP-2.10.app/Contents/MacOS/gimp"
 
 # python3 -B add_watermark.py --images_dir "/Users/${xxx}/Downloads/canon" \
@@ -50,7 +50,7 @@ python3 -B add_watermark.py --images_dir "/Users/${xxx}/Downloads/canon/2022_03_
 #     --font_name "FleurDeLeah" \
 #     --font_size 12 \
 #     --debug 1 \
-#     --has_gui 0
+#     --has_gui 0 \
 #     --gimp_path "/Applications/GIMP-2.10.app/Contents/MacOS/gimp"
 
 #FleurDeLeah
